2977ab92a7395d0e3e96c4b970379a5c88549b3c
Jose <mikomeka1@gmail.com>
2019-11-19 23:49:01 -0600

    Creacion FactoryCompon

---
+++ Battleship/src/battleship/FactoryComponente.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+/**
+ *
+ * @author mikom
+ */
+public class FactoryComponente {
+    public static Componente crearComponente(int num){
+        switch(num){
+            case 1:
+                
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
+    }    
+}
